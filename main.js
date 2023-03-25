14b22bf2545930167c899fb6a667a2459ffd154d
Mariana Alves <108038205+Marianalvs@users.noreply.github.com>
2023-03-25 13:21:38 -0300

    Add files via upload
    Aula de revisão Day 4 - DNC

---
+++ main.js
@@ -0,0 +1,17 @@
+let pets = [];
+
+
+
+function addPet(){
+
+    event.preventDefault();
+
+    let pet = {
+        "petName":document.getElementById('fname').value,
+        "petBreed":document.getElementById('fbreed').value,
+        "petOwner":document.getElementById('fowner').value,
+        "phone":document.getElementById('fphone').value,
+    }
+
+
+}
